5df8f2b4aa9b2302d5107cf80024b164ccc4d5c8
ks_meziane <ks_meziane@esi.dz>
2024-11-05 17:26:03 +0100

    fixed unused vars error v2

--- app/api/movies/now-playing/route.ts
+++ app/api/movies/now-playing/route.ts
@@ -8,13 +8,13 @@ export async function GET() {
     const response = await fetch(tmdbUrl);
     
     if (!response.ok) {
-      return NextResponse.json({ error: 'Failed to fetch data from TMDb' }, { status: 500 });
+      return NextResponse.json('Failed to fetch data from TMDb', { status: 500 });
     }
 
     const data = await response.json();
     
     return NextResponse.json(data);
   } catch (error) {
-    return NextResponse.json({ error: 'An error occurred while fetching movie data' }, { status: 500 });
+    return NextResponse.json('An error occurred while fetching movie data', { status: 500 });
   }
 }
