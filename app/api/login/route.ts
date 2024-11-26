511e79900afa0bd527dadb418604eb48f878e851
A-SELHI <abdouselhi31@gmail.com>
2024-11-27 00:40:59 +0100

    fixed next-auth issue prd 3

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -3,10 +3,11 @@ import { user } from '../../repository/users';
 
 export async function POST(request: Request): Promise<Response> {
     const { username, password } = await request.json();
-    console.log(username);
-    console.log(password);
+    
     if (username === user.username && bcrypt.compareSync(password, user.password)) {
         return new Response(JSON.stringify({ isAuthenticated: true }), { status: 200 });
+        console.log(username);
+    console.log(password);
     } else {
         return new Response(JSON.stringify({ isAuthenticated: false }), { status: 401 });
     }
