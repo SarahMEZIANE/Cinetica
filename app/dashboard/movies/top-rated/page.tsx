85df312660c04a9e76973bbc7d36d20d0cd0a18b
A-SELHI <abdouselhi31@gmail.com>
2024-11-27 19:41:11 +0100

    added movies to dashboard paginator

--- app/dashboard/movies/top-rated/page.tsx
+++ app/dashboard/movies/top-rated/page.tsx
@@ -25,16 +25,16 @@ const MoviesPage = () => {
         <button
           onClick={() => handlePageChange(currentPage - 1)}
           disabled={currentPage === 1}
-          className="px-4 py-2 bg-gray-200 text-gray-600 rounded-lg hover:bg-gray-300 disabled:opacity-50"
+          className="px-4 py-2 bg-gray-200 text-gray-600 rounded-lg dark:bg-white dark:text-black hover:bg-gray-300 disabled:opacity-50"
         >
           Previous
         </button>
-        <span className="text-lg font-semibold text-gray-700">
+        <span className="text-lg font-semibold text-gray-700 dark:text-white">
           Page {currentPage}
         </span>
         <button
           onClick={() => handlePageChange(currentPage + 1)}
-          className="px-4 py-2 bg-gray-200 text-gray-600 rounded-lg hover:bg-gray-300"
+          className="px-4 py-2 bg-gray-200 dark:bg-white dark:text-black text-gray-600 rounded-lg hover:bg-gray-300"
         >
           Next
         </button>
