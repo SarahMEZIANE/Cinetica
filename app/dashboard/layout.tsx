39cdd605bcdda096b75b7aafa4feda63f527ad9c
A-SELHI <abdouselhi31@gmail.com>
2024-12-14 00:34:12 +0100

    added search 2

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -3,6 +3,7 @@ import Sidebar from "@/app/dashboard/Sidebar";
 import Header from "@/app/dashboard/Header";
 import { useDashboardState } from "@/hooks/useDashboardState";
 import { useLogin } from "@/hooks/useLogin";
+import {Suspense} from "react";
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   const { sidebarOpen, toggleSidebar, active, setActive } = useDashboardState();
@@ -10,7 +11,9 @@ const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
 
   return (
     <div className="flex flex-col min-h-screen bg-gray-50 dark:bg-[#494949]">
+      <Suspense fallback={<div>Loading...</div>}>
       <Header sidebarOpen={sidebarOpen} toggleSidebar={toggleSidebar} />
+      </Suspense>
       <div className="flex flex-1">
         <Sidebar
           sidebarOpen={sidebarOpen}
